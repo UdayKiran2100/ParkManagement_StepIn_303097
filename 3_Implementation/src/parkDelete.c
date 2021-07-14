12e803aa22f45b1dc8cc50ae37b280e914cebe1c
UdayKiran2100 <uday.kiran2100@gmail.com>
2021-07-14 07:55:12 +0530

    commit8

--- 3_Implementation/src/parkDelete.c
+++ 3_Implementation/src/parkDelete.c
@@ -24,6 +24,10 @@ parkList* deleteIndividual(parkList *head, int token)
         printf("Park is Empty\n");
         return head;
     }
+    else if(head->token_number==token)
+    {
+        return NULL;
+    }
     else
     {
         parkList *temp=head;
@@ -33,7 +37,11 @@ parkList* deleteIndividual(parkList *head, int token)
             prev=temp;
             temp=temp->next;
         }
-        if(temp->next==NULL)
+        if(temp->next==NULL && temp->token_number==token)
+        {
+            return prev;
+        }
+        else if(temp->next==NULL)
         {
             printf("The individual with given token number is not present in park\n");
         }
---
+++ 3_Implementation/test/test_main.c
@@ -0,0 +1,151 @@
+#include "unity.h"
+#include "parkdef.h"
+#include "default_Libraries.h"
+#include "linuxconio.h"
+
+
+
+/* Modify these two lines according to the project */
+#define PROJECT_NAME  "ParkManagement"
+
+/* Prototypes for all the test functions */
+void test_parkadd(void);
+void test_parkdelete_beginning(void);
+void test_parkdelete(void);
+void test_print_toFile(void);
+void test_print(void);
+void test_pushtoken(void);
+void test_poptoken(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_parkadd);
+  RUN_TEST(test_parkdelete_beginning);
+  RUN_TEST(test_parkdelete);
+  RUN_TEST(test_print_toFile);
+  RUN_TEST(test_print);
+  RUN_TEST(test_pushtoken);
+  RUN_TEST(test_poptoken);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+
+void test_parkadd(void)
+{
+    char name[20]="Uday"; 
+    int token,time;
+    parkList *head = (parkList*)malloc(sizeof(parkList));
+    time=10002;
+    token=1;
+    head->in_time = time;
+    head->token_number=token;
+    strcpy(head->name,name);
+
+    TEST_ASSERT_EQUAL(head, addIndividual(head,name,2,10005));   
+}
+
+void test_parkdelete_beginning(void)
+{
+  parkList *temp1 = (parkList*)malloc(sizeof(parkList));
+  parkList *temp2 = (parkList*)malloc(sizeof(parkList));
+  temp1->next=temp2;
+
+  TEST_ASSERT_EQUAL(temp2, deleteIndividual_beginning(temp1));
+
+}
+
+void test_parkdelete(void)
+{
+  parkList *temp1 = NULL;
+  
+  TEST_ASSERT_EQUAL(temp1, deleteIndividual(temp1,1));
+
+  temp1=(parkList*)malloc(sizeof(parkList));
+  temp1->token_number=2;
+  
+  TEST_ASSERT_EQUAL(temp1, deleteIndividual(temp1,1));
+
+  TEST_ASSERT_EQUAL(NULL, deleteIndividual(temp1,2));
+
+  parkList *temp2 = (parkList*)malloc(sizeof(parkList));
+  temp2->token_number=1;
+
+  TEST_ASSERT_EQUAL(temp1, deleteIndividual(temp1,1));
+
+}
+
+void test_print_toFile(void)
+{
+  parkList *temp1 = NULL;
+  
+  TEST_ASSERT_EQUAL(0, print_parkdetails_toFile(temp1));
+
+  temp1=(parkList*)malloc(sizeof(parkList));
+  temp1->token_number=2;
+  temp1->in_time=1001;
+  strcpy(temp1->name , "Uday");
+
+  TEST_ASSERT_EQUAL(1, print_parkdetails_toFile(temp1));
+  
+}
+
+
+void test_print(void)
+{
+  parkList *temp1 = NULL;
+  
+  TEST_ASSERT_EQUAL(0, print_parkdetails(temp1));
+
+  temp1=(parkList*)malloc(sizeof(parkList));
+  temp1->token_number=2;
+  temp1->in_time=1001;
+  strcpy(temp1->name , "Uday");
+
+  TEST_ASSERT_EQUAL(1, print_parkdetails(temp1));
+  
+}
+
+void test_pushtoken(void)
+{
+  tokenStack stack;
+  stack.tokenno[0]=1;
+  stack.tokenno[1]=2;
+  stack.tokenno[2]=3;
+  stack.top=2;
+
+  TEST_ASSERT_EQUAL(3, push_token(&stack,4));
+  
+  stack.top=99;
+  TEST_ASSERT_EQUAL(0, push_token(&stack,5));
+
+}
+
+
+void test_poptoken(void)
+{
+  tokenStack stack;
+  stack.tokenno[0]=1;
+  stack.tokenno[1]=2;
+  stack.tokenno[2]=3;
+  stack.top=2;
+
+  TEST_ASSERT_EQUAL(3, pop_token(&stack));
+  
+  stack.top=-1;
+  TEST_ASSERT_EQUAL(-1, pop_token(&stack));
+  
+}
+
